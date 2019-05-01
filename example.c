f508a5dc83222be1d4542ea99f3419295db9c527
Caswall Engelsman <caswallengelsman6@gmail.com>
2019-05-01 21:32:48 +0000

    Redesigned unitest backend

--- example.c
+++ example.c
@@ -7,24 +7,41 @@ void print(char* a){
 }
 
 int main(void){
-	TEST(kick,
-		char a[] = "Kick";
-		char b[] = "Ness";
-		T_ASSERT(1 == 0);
-		T_ASSERT_STRING(a, b);
-	);
-
-	TEST(helloworld,
+	N_TEST(Negative testing,
+		long long int a = 1 << 31;
+		long long int b = a*a;
 		T_ASSERT(1 < 0);
 		T_ASSERT_NUM(1, 0);
+		T_ASSERT_NUM(b, 0);
+
+
+		char n[] = "Kick";
+		char m[] = "Ness";
+		T_ASSERT_STRING(n, m);
 	);
 
-	TEST(foo, 
-		T_ASSERT(1 > 0);
+	TEST(String tests,
+		char a[] = "nbionob <F9>82r32h9nrwn 9h489h 4<F9><F10><F3><F2>niom o";
+		char b[] = "nbionob <F9>82r32h9nrwn 9h489h 4<F9><F10><F3><F2>niom o";
+		T_ASSERT_STRING(a, b);
 		T_ASSERT_STRING("Kickness", "Kickness");
 	);
 
-	TEST(Print something, print("g"));
+	TEST(Integer tests,
+		long long int a = 1 << 31;
+		long long int b = a*a;
+		T_ASSERT(1 == 1);
+		T_ASSERT_NUM(1, 1);
+		T_ASSERT_NUM(b, a*a);
+	);
+
+	TEST(Floating point tests,
+		T_ASSERT_FLOAT(0.5, 0.5f);
+		T_ASSERT_FLOAT((float) 22/7, (float) 22/7);
+		T_ASSERT_FLOAT((double) 22/7, (double) 22/7);
+	);
+
+	TEST(Print 'g', print("g"));
 	
 	T_CONCLUDE();
 	return 0;
--- unitest.h
+++ unitest.h
@@ -3,37 +3,57 @@
 
 #define T_SET_COLOUR(stream, colour) fprintf(stream, "\033[0;"#colour"m");
 #define T_RESET_COLOUR(stream) fprintf(stream, "\033[0m");
-#define FAILED(msg) \
+#define FAILED(msg, a, b) \
 		T_FLAG = 1; \
 		T_SET_COLOUR(stderr, 31); \
-		fprintf(stderr, "Failed %s:%d: %s\n", __FILE__, __LINE__, #msg); \
-		T_RESET_COLOUR(stderr)
+		fprintf(stderr, "Failed %s:%d: "#msg"\n", __FILE__, __LINE__, (a), (b)); \
+		T_RESET_COLOUR(stderr) \
+
+#define PASSED(msg) \
+		T_SET_COLOUR(stdout, 32); \
+		printf("PASSED %s\n", msg); \
+		T_RESET_COLOUR(stdout); \
+		T_PASSED++ \
 
 /** This Macro does not work with common seperated initialising statements */
-#define TEST(name, code) T_FLAG = 0; T_COUNT++; \
-					printf("TEST: %s\n", #name); \
+#define TEST(name, code) NEG_FLAG = 1; MAIN_TEST(POSITIVE, name, code)
+#define N_TEST(name, code) NEG_FLAG = 0; MAIN_TEST(NEGATIVE, name, code)
+
+#define MAIN_TEST(title, name, code) T_FLAG = 0; T_COUNT++; \
+					printf(#title" TEST: %s\n", #name); \
 					{code;} \
 					if(!T_FLAG){ \
-						T_SET_COLOUR(stdout, 32); \
-						printf("PASSED\n"); \
-						T_RESET_COLOUR(stdout); \
-						T_PASSED++; \
+						PASSED(""); \
 					} \
 					printf("\n")
 
+#define ASSERT(statement, op1, op2, format) \
+	if(statement ^ NEG_FLAG){ FAILED(format, op1, op2); }
+
 #define T_ASSERT(statement) \
-	if(!(statement)){ FAILED(statement); }
+	ASSERT(statement, #statement, "", "%s%s")
 
 #define T_ASSERT_STRING(a, b) \
-	if(strcmp((a), (b))){ FAILED(a != b); }
+	ASSERT(!strcmp((a), (b)), (a), (b), "%s != %s")
 
 #define T_ASSERT_NUM(a, b) \
-	if((a) != (b)){ FAILED(a != b); }
+	if(sizeof((a)) <= sizeof(int)){ \
+		ASSERT((a) == (b), (a), (b), "%d != %d"); \
+	}else if(sizeof((a)) == sizeof(long)){ \
+		ASSERT((a) == (b), (a), (b), "%ld != %ld"); \
+	}
+
+#define T_ASSERT_FLOAT(a, b) \
+	if(sizeof((a)) == sizeof(float)){ \
+		ASSERT((a) == (b), (a), (b), "%f != %f"); \
+	}else if(sizeof((a)) == sizeof(double)){ \
+		ASSERT((a) == (b), (a), (b), "%lf != %lf"); \
+	}
 
 #define T_CONCLUDE() \
 		printf("%d/%d PASSED\n", T_PASSED, T_COUNT);
 
-char T_FLAG;
+char T_FLAG, NEG_FLAG;
 int T_COUNT = 0;
 int T_PASSED = 0;
 
